62dc9bdcfcff562f370a806d2bf7cd1cac3f9d45
CapacitorSet <capacitorset@gmail.com>
2020-08-17 21:14:12 +0000

    Add polybar

--- launch_polybar.sh
+++ launch_polybar.sh
@@ -1,7 +1,7 @@
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload -c ~/dotfiles/polybar.conf top &
+    MONITOR=$m polybar --reload -c ~/dotfiles/polybar.conf main &
   done
 else
-  polybar --reload -c ~/dotfiles/polybar.conf top &
+  polybar --reload -c ~/dotfiles/polybar.conf main &
 fi
